ab6030354403dd72995c34609bd3bc9384ceef8e
Nitika Chaudhary <115881318+Nitika-Chaudhary@users.noreply.github.com>
2024-09-23 12:23:22 +0200

    do some changes in App.js and made some components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div><img/></div>
+      <div>copyright</div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Header = () => {
+  return (
+    <div>
+        <div><img src='' alt='logo'/></div>
+      <Nav/>
+    </div>
+  )
+}
+
+export default Header
