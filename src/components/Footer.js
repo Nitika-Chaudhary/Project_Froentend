defd7ab9a6208f07829198aafb68c9b41f07e52b
Nitika Chaudhary <115881318+Nitika-Chaudhary@users.noreply.github.com>
2024-09-24 12:51:55 +0200

    Do some changes in nav and footer file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Nav from './Nav'
 
 const Footer = () => {
   return (
     <div>
-      <div><img/></div>
-      <div>copyright</div>
+      <div><img src='Restaurant -logo'/></div>
+      <div><Nav/></div>
+      <div>Contact</div>
+      <div>Social Media Links</div>
     </div>
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,11 +5,13 @@ const Nav = () => {
     <div>
       <nav>
 <ul>
-    <li><a href=''></a></li>
-    <li><a href=''></a></li>
-    <li><a href=''></a></li>
-    <li><a href=''></a></li>
-    <li><a href=''></a></li>
+    <li><a href=''>Home</a></li>
+    <li><a href=''>About</a></li>
+    <li><a href=''>Menu</a></li>
+    <li><a href=''>Reservation</a></li>
+    <li><a href=''>Order Online</a></li>
+    <li><a href=''>Login</a></li>
+
     
 </ul>
       </nav>
